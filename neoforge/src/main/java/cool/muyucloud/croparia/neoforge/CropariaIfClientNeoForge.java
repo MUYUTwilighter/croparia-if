4b3081b9a5631c6354992dbbf797476e78f59179
MUYU_Twilighter <1484605372@qq.com>
2026-02-02 21:55:00 +0800

    Fix redundant annotation field

--- neoforge/src/main/java/cool/muyucloud/croparia/neoforge/CropariaIfClientNeoForge.java
+++ neoforge/src/main/java/cool/muyucloud/croparia/neoforge/CropariaIfClientNeoForge.java
@@ -7,7 +7,7 @@ import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 
-@EventBusSubscriber(modid = CropariaIf.MOD_ID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+@EventBusSubscriber(modid = CropariaIf.MOD_ID, value = Dist.CLIENT)
 public class CropariaIfClientNeoForge {
     @SubscribeEvent()
     public static void onClientSetup(FMLClientSetupEvent event) {
