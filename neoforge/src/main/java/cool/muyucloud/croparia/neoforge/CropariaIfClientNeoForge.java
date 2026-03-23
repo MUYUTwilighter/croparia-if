cc3c1be2b08166d471ddd5c28280c8faf29f9186
MUYU_Twilighter <1484605372@qq.com>
2026-03-23 23:08:25 +0800

    fix(registry): Fix screen registration failure by Arch API

--- neoforge/src/main/java/cool/muyucloud/croparia/neoforge/CropariaIfClientNeoForge.java
+++ neoforge/src/main/java/cool/muyucloud/croparia/neoforge/CropariaIfClientNeoForge.java
@@ -3,12 +3,15 @@ package cool.muyucloud.croparia.neoforge;
 import cool.muyucloud.croparia.CropariaIf;
 import cool.muyucloud.croparia.api.element.Element;
 import cool.muyucloud.croparia.client.CropariaIfClient;
+import cool.muyucloud.croparia.client.screen.CropTransmuterScreen;
+import cool.muyucloud.croparia.registry.MenuTypes;
 import dev.architectury.core.fluid.ArchitecturyFluidAttributes;
 import net.minecraft.resources.ResourceLocation;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.neoforge.client.event.RegisterMenuScreensEvent;
 import net.neoforged.neoforge.client.extensions.common.IClientFluidTypeExtensions;
 import net.neoforged.neoforge.client.extensions.common.RegisterClientExtensionsEvent;
 import net.neoforged.neoforge.fluids.FluidType;
@@ -26,6 +29,11 @@ public class CropariaIfClientNeoForge {
         for (Element element : Element.values()) {
             if (element.shouldLoad()) registerFluidClientExtensions(event, element.getFluidAttr());
         }
+    }
+
+    @SubscribeEvent
+    public static void onRegisterScreens(RegisterMenuScreensEvent event) {
+        event.register(MenuTypes.CROP_TRANSMUTER.get(), CropTransmuterScreen::new);
     }
 
     private static void registerFluidClientExtensions(
