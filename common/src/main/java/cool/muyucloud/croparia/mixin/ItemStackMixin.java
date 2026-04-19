99a573a746f3dbb245cad74fb6cc1465be77e2c3
MUYU_Twilighter <1484605372@qq.com>
2026-04-19 18:21:57 +0800

    remove redundant mixin

--- common/src/main/java/cool/muyucloud/croparia/mixin/ItemStackMixin.java
+++
@@ -1,8 +0,0 @@
-package cool.muyucloud.croparia.mixin;
-
-import net.minecraft.world.item.ItemStack;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(ItemStack.class)
-public abstract class ItemStackMixin {
-}
