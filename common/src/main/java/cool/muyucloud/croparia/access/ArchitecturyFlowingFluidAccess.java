a691139f1dcb52939ce0acb0e29d6c9761cb6457
MUYU_Twilighter <1484605372@qq.com>
2026-04-23 22:52:09 +0800

    remove redundant mixin access

--- common/src/main/java/cool/muyucloud/croparia/access/ArchitecturyFlowingFluidAccess.java
+++
@@ -1,7 +0,0 @@
-package cool.muyucloud.croparia.access;
-
-import dev.architectury.core.fluid.ArchitecturyFluidAttributes;
-
-public interface ArchitecturyFlowingFluidAccess {
-    ArchitecturyFluidAttributes cif$getAttributes();
-}
