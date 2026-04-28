9870475e37ec922d31252dbea006e6ad6a79200e
MUYU_Twilighter <1484605372@qq.com>
2026-04-28 10:38:43 +0800

    optimize error printing

--- fabric/src/main/java/cool/muyucloud/croparia/api/generator/pack/fabric/PackHandlerImpl.java
+++ fabric/src/main/java/cool/muyucloud/croparia/api/generator/pack/fabric/PackHandlerImpl.java
@@ -1,5 +1,6 @@
 package cool.muyucloud.croparia.api.generator.pack.fabric;
 
+import cool.muyucloud.croparia.CropariaIf;
 import net.fabricmc.loader.api.FabricLoader;
 
 import java.io.File;
@@ -9,12 +10,11 @@ import java.util.function.BiConsumer;
 public class PackHandlerImpl {
     public static void forEachJar(BiConsumer<File, String> consumer) {
         FabricLoader.getInstance().getAllMods().forEach(mod -> {
+            String modId = mod.getMetadata().getId();
             try {
-                mod.getOrigin().getPaths().forEach(path -> {
-                    consumer.accept(path.toFile(), mod.getMetadata().getId());
-                });
-            } catch (UnsupportedOperationException ignored) {
-                // JIJ does not support getOrigin().getPaths()
+                mod.getOrigin().getPaths().forEach(path -> consumer.accept(path.toFile(), modId));
+            } catch (UnsupportedOperationException e) {
+                CropariaIf.LOGGER.debug("Cannot scan generators from mod %s".formatted(modId), e);
             }
         });
     }
--- neoforge/src/main/java/cool/muyucloud/croparia/api/generator/pack/neoforge/PackHandlerImpl.java
+++ neoforge/src/main/java/cool/muyucloud/croparia/api/generator/pack/neoforge/PackHandlerImpl.java
@@ -1,5 +1,6 @@
 package cool.muyucloud.croparia.api.generator.pack.neoforge;
 
+import cool.muyucloud.croparia.CropariaIf;
 import net.neoforged.fml.ModList;
 
 import java.io.File;
@@ -8,6 +9,13 @@ import java.util.function.BiConsumer;
 @SuppressWarnings("unused")
 public class PackHandlerImpl {
     public static void forEachJar(BiConsumer<File, String> consumer) {
-        ModList.get().getMods().forEach(mod -> consumer.accept(mod.getOwningFile().getFile().getFilePath().toFile(), mod.getModId()));
+        ModList.get().getMods().forEach(mod -> {
+            String modId = mod.getModId();
+            try {
+                consumer.accept(mod.getOwningFile().getFile().getFilePath().toFile(), mod.getModId());
+            } catch (UnsupportedOperationException e) {
+                CropariaIf.LOGGER.debug("Cannot scan generators from mod %s".formatted(modId));
+            }
+        });
     }
 }
