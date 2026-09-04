9ca89b134ba8e44ee9e4244d5ba2eb6014c5892e
MUYU_Twilighter <1484605372@qq.com>
2026-09-04 10:15:03 +0100

    greenhouse optimization

--- common/src/main/java/cool/muyucloud/croparia/api/core/block/entity/GreenhouseBlockEntity.java
+++ common/src/main/java/cool/muyucloud/croparia/api/core/block/entity/GreenhouseBlockEntity.java
@@ -104,7 +104,7 @@ public class GreenhouseBlockEntity extends BlockEntity implements MenuProvider, 
         // Deposit
         if (!this.tryDeposit(droppedStacks)) return;    // Not enough space, abort
         // Remove melon
-        level.removeBlock(melonPos, false);
+        level.destroyBlock(melonPos, false);
     }
 
     public void tryHarvestBerry(ServerLevel level, BlockState berry, BlockPos berryPos) {
@@ -121,7 +121,7 @@ public class GreenhouseBlockEntity extends BlockEntity implements MenuProvider, 
     public void tryHarvestBreakable(ServerLevel level, BlockState breakable, BlockPos breakablePos) {
         List<ItemStack> droppedStacks = Block.getDrops(breakable, level, breakablePos, level.getBlockEntity(breakablePos));
         if (!this.tryDeposit(droppedStacks)) return;    // Not enough space, abort
-        level.removeBlock(breakablePos, false);
+        level.destroyBlock(breakablePos, false);
     }
 
     @SuppressWarnings("BooleanMethodIsAlwaysInverted")
